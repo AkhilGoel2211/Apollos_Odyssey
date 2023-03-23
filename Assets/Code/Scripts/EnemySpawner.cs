ddcc83747a4e7f504a47dcec70f2c2f47ac8dcd9
Akhil Goel <akhil1goel@gmail.com>
2023-03-23 16:18:26 +0530

    spawner done

---
+++ Assets/Code/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+
+    [SerializeField]
+    private float spawnInterval = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(spawnInterval, enemy));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-4f, 8f), Random.Range(0.5f, 6f), Random.Range(0f, 4f)), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+}
